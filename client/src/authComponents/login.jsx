391ce9c120e7d9a420efab209091b789fe771ce4
Abhi Passi <abhijit36254@gmail.com>
2025-07-02 23:15:29 +0530

    added login and register components

---
+++ client/src/authComponents/login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <>
+    <div>login</div>
+    <h1>HIII</h1>
+    </>
+  )
+}
+
+export default Login
---
+++ client/src/authComponents/register.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Register() {
+    console.log("hii");
+    
+  return (
+    <div>register</div>
+  )
+}
+
+export default Register
