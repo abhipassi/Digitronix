1b103eb32f3a1c2346294b4d573854680580754f
Abhi Passi <abhijit36254@gmail.com>
2025-07-02 23:16:04 +0530

    create nav and home

---
+++ client/src/components/nav.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+function Nav() {
+  return (
+
+    <>
+    <div className='flex justify-between items-center cursor-pointer h-12 shadow-md pr-5 pl-5'>
+        <div className='flex'>
+        <p className='text-lg font-bold'>Tech Gadget</p>
+    </div>
+
+    <div className='flex gap-4'> 
+         <Link to='/' className='cursor-pointer'>Home</Link>
+         <Link to='/login' className='cursor-pointer'>Shop</Link>
+         <Link to='#' className='cursor-pointer'>Support</Link>
+    </div>
+    </div>
+    
+    </>
+  )
+}
+
+export default Nav
