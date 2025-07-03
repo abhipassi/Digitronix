9d0dfdd4db35d35c111b432e2ba90c3575187616
Abhi Passi <abhijit36254@gmail.com>
2025-07-03 23:21:37 +0530

    routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import Login from './authComponents/login.jsx';
 import Register from './authComponents/register.jsx';
 import Home from './components/home.jsx';
+import OtpModal from './authComponents/otpModal.jsx';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/otp" element={<OtpModal />} />
     </Routes>
   );
 }
