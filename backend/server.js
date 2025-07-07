6cc1dd2bd05e4ccbcf92ced47d69c5f74ecbf502
Abhi Passi <abhijit36254@gmail.com>
2025-07-07 23:18:23 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import authRoutes from './routes/authRoutes.js'
 dotenv.config()
 const app =express()
 
-
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
