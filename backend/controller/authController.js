b9b5cd6a90f929c9234c336e8a3aeea5b68bc6cc
Abhi Passi <abhijit36254@gmail.com>
2025-07-07 23:17:29 +0530

    login fucntion

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,7 +1,24 @@
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcryptjs'
 
-
+// route for checking 
 export const demo = (req, res) => {
   res.json('Live');
 };
+
+// function for user login 
+export const userLogin = (req,res) =>{
+  let {email, password} = req.body
+  res.json("Done")
+  console.log(req.body);
+}
+
+
+// function for user sign up 
+
+export const userSignUp = (req,res) =>{
+  let {fullname, email, password} = req.body
+  res.json("Done")
+  console.log(req.body);
+  
+}
