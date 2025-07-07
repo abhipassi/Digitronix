b52a239cefd1cb0d99827c07037c6bc549b8cd26
Abhi Passi <abhijit36254@gmail.com>
2025-07-07 23:17:41 +0530

    login fucntion

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,10 +3,26 @@ const router = express.Router()
 
 
 import {
-    demo
+    demo,
+    userLogin,
+    userSignUp
 } from '../controller/authController.js'
 
+// public routes 
+
+// route for checking the server
 router.get('/demo', demo)
+
+// route for login 
+router.post('/login', userLogin )
+
+// router of SignUp 
+
+router.post('/register', userSignUp)
+
+
+// protected routes
+
 
 
 export default router;
