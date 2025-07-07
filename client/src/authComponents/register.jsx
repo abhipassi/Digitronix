927b9fa7dafdcd52ea8284b0fc00089e394271f1
Abhi Passi <abhijit36254@gmail.com>
2025-07-07 23:19:07 +0530

    making login ui

--- client/src/authComponents/register.jsx
+++ client/src/authComponents/register.jsx
@@ -1,8 +1,44 @@
+import axios from 'axios'
 import React from 'react'
+import { useState } from 'react'
 
 function Register() {
-    console.log("hii");
+  const [fullname, setFullName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
     
+
+  const handleFullName = (e) =>{
+    setFullName(e.target.value)
+  }
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value)
+  }
+  
+  const handlePassword = (e) => {
+    setPassword(e.target.value)
+  }
+
+  const handleConfirmPassword = (e) =>{
+    setConfirmPassword(e.target.value)
+  }
+
+  const handleSubmit = async (e) =>{
+    e.preventDefault()
+    let data = {fullname, email, password}
+    console.log(data);
+    
+    try {
+      let response = await axios.post('http://localhost:5000/register',data)
+      console.log(response);
+      
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }
   return (
     <>
       <div>
@@ -11,32 +47,26 @@ function Register() {
       <br />
       <br /><br />
       <br />
-      <form class="max-w-sm mx-auto">
-        <div class="mb-5">
-          <label for="text" class="block mb-2 text-sm font-medium text-gray-900 ">Full name</label>
-          <input type="text" id="" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your Full Name " required />
+      <form className="max-w-sm mx-auto" onSubmit={handleSubmit}>
+        <div className="mb-5">
+          <label htmlFor="fullname" className="block mb-2 text-sm font-medium text-gray-900 ">Full name</label>
+          <input type="text" onChange={handleFullName} id="fullname" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your Full Name " required />
         </div>
-        <div class="mb-5">
-          <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">Email Address</label>
-          <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your Email " required />
+        <div className="mb-5">
+          <label htmlFor="email"  className="block mb-2 text-sm font-medium text-gray-900 ">Email Address</label>
+          <input type="email" onChange={handleEmail} id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your Email " required />
         </div>
-        <div class="mb-5">
-          <label for="password" class="block mb-2 text-sm font-medium text-gray-900 ">Your password</label>
-          <input type="password" placeholder="Create Password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+        <div className="mb-5">
+          <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 ">Your password</label>
+          <input type="password" onChange={handlePassword} placeholder="Create Password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
         </div>
 
-        <div class="mb-5">
-          <label for="password" class="block mb-2 text-sm font-medium text-gray-900 ">Confirm Password</label>
-          <input type="password" placeholder="Confirm password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+        <div className="mb-5">
+          <label htmlFor="confirmPassword"  className="block mb-2 text-sm font-medium text-gray-900 ">Confirm Password</label>
+          <input type="password" onChange={handleConfirmPassword} placeholder="Confirm password" id="confirmPassword" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
         </div>
-        {/* <div class="flex items-start mb-5">
-    <div class="flex items-center h-5">
-      <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded-sm bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
-    </div>
-    <label for="remember" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
-  </div> */}
 
-        <button type="submit" class="text-white bg-blue-200  hover:bg-blue-300 focus:ring-2 focus:outline-none font-medium rounded-lg text-sm w-full  px-5 py-2.5 text-center dark:focus:ring-blue-400">Sign Up</button>
+        <button type="submit" className="text-white bg-blue-200  hover:bg-blue-300 focus:ring-2 focus:outline-none font-medium rounded-lg text-sm w-full  px-5 py-2.5 text-center dark:focus:ring-blue-400">Sign Up</button>
       </form>
 
     </>
