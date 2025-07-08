53f81e821694c439981577ea159883712dd5e58b
Abhi Passi <abhijit36254@gmail.com>
2025-07-08 22:32:20 +0530

    updated routes

--- client/src/authComponents/register.jsx
+++ client/src/authComponents/register.jsx
@@ -46,7 +46,7 @@ function Register() {
       </div>
       <br />
       <br /><br />
-      <br />
+  
       <form className="max-w-sm mx-auto" onSubmit={handleSubmit}>
         <div className="mb-5">
           <label htmlFor="fullname" className="block mb-2 text-sm font-medium text-gray-900 ">Full name</label>
@@ -66,7 +66,7 @@ function Register() {
           <input type="password" onChange={handleConfirmPassword} placeholder="Confirm password" id="confirmPassword" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
         </div>
 
-        <button type="submit" className="text-white bg-blue-200  hover:bg-blue-300 focus:ring-2 focus:outline-none font-medium rounded-lg text-sm w-full  px-5 py-2.5 text-center dark:focus:ring-blue-400">Sign Up</button>
+        <button type="submit" className="text-white bg-blue-200 hover:bg-blue-300 focus:ring-2 focus:outline-none font-medium rounded-lg text-sm w-full  px-5 py-2.5 text-center dark:focus:ring-blue-400">Sign Up</button>
       </form>
 
     </>
