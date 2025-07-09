fe7a0e18a7ad093fa28160eaf9d1c84b07d01f88
Abhi Passi <abhijit36254@gmail.com>
2025-07-09 23:15:11 +0530

    updated

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,15 +1,16 @@
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcryptjs'
-import userModel from '../models/user'
+import userModel from '../models/user.js'
+
 
 // route for checking 
-export const demo = (req,res) => {
+export const demo = (req, res) => {
   res.json('Live');
 };
 
 // function for user login 
-export const userLogin = (req,res) =>{
-  let {email, password} = req.body
+export const userLogin = (req, res) => {
+  let { email, password } = req.body
   res.json("Done")
   console.log(req.body);
 }
@@ -17,15 +18,37 @@ export const userLogin = (req,res) =>{
 
 // function for user sign up 
 
-export const userSignUp = async (req,res) =>{
-  let {fullname, email, password} = req.body
+export const userSignUp = async (req, res) => {
+  let { fullname, email, password } = req.body
+  console.log(req.body);
 
-  if(!fullname || !email || ! password){
-     return res.status(404).json("Kindly Provide the data")
+  if (!fullname || !email || !password) {
+    return res.status(404).json("Kindly Provide the data")
   }
 
+  try {
 
-  res.json("Done")
-  console.log(req.body);
-  
+    const isExistingUser = await userModel.findOne({ $or: [{ fullname }, { email }] })
+
+    if (isExistingUser) return res.status(400).json({ msg: "User already exist" })
+
+    const verificationCode = Math.floor(100000 + Math.random() * 900000).toString();
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const hashedverificationCode = await bcrypt.hash(verificationCode, 10)
+
+    await userModel.create({
+      fullname:fullname,
+      email:email,
+      password: hashedPassword,
+      verificationCode: hashedverificationCode
+    })
+    res.status(200).json({ msg: "User Created Successfully" })
+    console.log("Done");
+  }
+  catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+
 }
