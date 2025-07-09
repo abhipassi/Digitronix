7275917dfdbaf25285d8f01a464ecbec175e6001
Abhi Passi <abhijit36254@gmail.com>
2025-07-09 23:15:30 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,10 @@ import dotenv from'dotenv'
 import cors from 'cors'
 import cookieParser from 'cookie-parser';
 import path from 'path'
-import { log } from 'console';
 import authRoutes from './routes/authRoutes.js'
 
 dotenv.config()
-const app =express()
+const app = express()
 
 app.use(cors())
 app.use(express.json())
