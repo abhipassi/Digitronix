f0f029d78fdb69ba773710118a95a78e4138bc10
Abhi Passi <abhijit36254@gmail.com>
2025-07-09 23:15:48 +0530

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,21 @@ import Register from './authComponents/register.jsx';
 import Home from './components/home.jsx';
 import OtpModal from './authComponents/otpModal.jsx';
 import { Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
+    <>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/otp" element={<OtpModal />} />
     </Routes>
+    <ToastContainer position="top-right" autoClose={4000} limit={1} />
+    </>
+
   );
 }
 
