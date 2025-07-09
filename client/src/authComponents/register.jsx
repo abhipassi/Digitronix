44c62e6afb9af46a58895197a42152efa5e121f7
Abhi Passi <abhijit36254@gmail.com>
2025-07-09 23:15:59 +0530

    updated

--- client/src/authComponents/register.jsx
+++ client/src/authComponents/register.jsx
@@ -1,42 +1,64 @@
 import axios from 'axios'
 import React from 'react'
 import { useState } from 'react'
+import { toast } from 'react-toastify';
+
 
 function Register() {
   const [fullname, setFullName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
-    
 
-  const handleFullName = (e) =>{
+
+  let regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const passRegex = new RegExp("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&\\-+=()])(?=\\S+$).{8,20}$");
+  let passError = "Password must be 8-20 characters long and include at least one uppercase letter, one lowercase letter, one digit, and one special character.No spaces allowed"
+
+  const handleFullName = (e) => {
     setFullName(e.target.value)
   }
 
   const handleEmail = (e) => {
     setEmail(e.target.value)
   }
-  
+
   const handlePassword = (e) => {
     setPassword(e.target.value)
   }
 
-  const handleConfirmPassword = (e) =>{
+  const handleConfirmPassword = (e) => {
     setConfirmPassword(e.target.value)
   }
 
-  const handleSubmit = async (e) =>{
+
+  const handleSubmit = async (e) => {
     e.preventDefault()
-    let data = {fullname, email, password}
+    let data = { fullname, email, password }
     console.log(data);
-    
-    try {
-      let response = await axios.post('http://localhost:5000/register',data)
-      console.log(response);
-      
-    } catch (error) {
-      console.log(error);
-      
+
+    if (password !== confirmPassword) {
+      return toast.error("Password does not match!")
+    }
+
+    else if (!regex.test(email)) {
+      return toast.error("Kindly enter valid email address")
+    }
+
+    else if (!passRegex.test(password)) {
+      return toast.error(passError)
+    }
+    else {
+      try {
+        let response = await axios.post('http://localhost:5000/register', data)
+        if (response.status === 200 || response.status === 201) return toast.success("Welcome")
+
+        console.log(response);
+
+      } catch (error) {
+        console.log(error);
+
+      }
     }
   }
   return (
@@ -46,14 +68,14 @@ function Register() {
       </div>
       <br />
       <br /><br />
-  
+
       <form className="max-w-sm mx-auto" onSubmit={handleSubmit}>
         <div className="mb-5">
           <label htmlFor="fullname" className="block mb-2 text-sm font-medium text-gray-900 ">Full name</label>
           <input type="text" onChange={handleFullName} id="fullname" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your Full Name " required />
         </div>
         <div className="mb-5">
-          <label htmlFor="email"  className="block mb-2 text-sm font-medium text-gray-900 ">Email Address</label>
+          <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 ">Email Address</label>
           <input type="email" onChange={handleEmail} id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your Email " required />
         </div>
         <div className="mb-5">
@@ -62,7 +84,7 @@ function Register() {
         </div>
 
         <div className="mb-5">
-          <label htmlFor="confirmPassword"  className="block mb-2 text-sm font-medium text-gray-900 ">Confirm Password</label>
+          <label htmlFor="confirmPassword" className="block mb-2 text-sm font-medium text-gray-900 ">Confirm Password</label>
           <input type="password" onChange={handleConfirmPassword} placeholder="Confirm password" id="confirmPassword" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
         </div>
 
