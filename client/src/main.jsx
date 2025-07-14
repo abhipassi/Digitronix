5615cc8498a1d31c3a9f49a1315d1a0eb02ae7a7
Abhi Passi <abhijit36254@gmail.com>
2025-07-14 22:36:56 +0530

    updated

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Nav from './components/nav.jsx';
-
 import { BrowserRouter } from 'react-router-dom'
+import { Import } from 'lucide-react';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
