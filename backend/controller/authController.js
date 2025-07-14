4172c1ebe902d8da34bdfd43175d76c0a0c8ae41
Abhi Passi <abhijit36254@gmail.com>
2025-07-14 12:58:36 +0530

    added otp route

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcryptjs'
 import userModel from '../models/user.js'
+import { sendVerificationCode } from '../middleware/email.js';
 
 
 // route for checking 
@@ -39,16 +40,58 @@ export const userSignUp = async (req, res) => {
     const hashedverificationCode = await bcrypt.hash(verificationCode, 10)
 
     await userModel.create({
-      fullname:fullname,
-      email:email,
+      fullname: fullname,
+      email: email,
       password: hashedPassword,
       verificationCode: hashedverificationCode
     })
     res.status(200).json({ msg: "User Created Successfully" })
-    console.log("Done");
+    // console.log("Done");
+    try {
+      sendVerificationCode(email, verificationCode);
+    } catch (emailErr) {
+      res.status(201).json({
+        msg: "User registered, but verification email failed to send",
+        emailError: emailErr.message,
+      });
+    }
+
   }
   catch (error) {
     res.status(500).json({ error: error.message })
   }
-
 }
+
+
+export const otpVerification = async (req, res) => {
+  const { email, verificationCode } = req.body
+
+  if (!verificationCode) {
+    res.status(400).json({ message: 'verification code is required.' })
+  }
+
+  try {
+    const user = await userModel.findOne({ email: email })
+    const encryptedCode = user.verificationCode
+
+    const isMatch = await bcrypt.compare(verificationCode, encryptedCode)
+
+    if (isMatch) {
+      const token = jwt.sign({ email: email }, process.env.JWT_SECRET)
+      res.cookie('token', token)
+      return res.status(200).json({ message: "Verified" })
+    }
+    else {
+      return res.status(404).json({ message: "Invalid or expired verification code." })
+    }
+  } catch (error) {
+    console.error('OTP verification error:', error);
+    return res.status(500).json({ message: 'Server error during verification.' });
+  }
+}
+
+// export const getEmailForVerification =  async (req,res) =>{
+//   let {email} =req.body
+//   console.log(req.body);
+//   res.json("Got it")
+// }
