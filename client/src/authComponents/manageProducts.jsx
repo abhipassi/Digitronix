b790bd57a617032c942aec7f011a6c5bc5cbc6b2
Abhi Passi <abhijit36254@gmail.com>
2025-07-23 12:37:11 +0530

    created manage product components

---
+++ client/src/authComponents/manageProducts.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Sidebar from './sidebar'
+function ManageProducts() {
+  return (
+    <Sidebar >  
+    <div>ManageProducts</div>
+    </Sidebar>
+  )
+}
+
+export default ManageProducts
