0bbcef6991e95fa63a0cdcd82a2a61ff91ef66a5
Abhi Passi <abhijit36254@gmail.com>
2025-07-24 23:54:00 +0530

    added routes and navigaton for the admin components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import { Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+
 // admin sidebar components 
 import Products from './authComponents/products.jsx';
 import Customers from './authComponents/customers.jsx';
@@ -14,21 +15,28 @@ import AdminDasboard from './authComponents/adminDashboard.jsx';
 import Order from './authComponents/order.jsx';
 import Sidebar from './authComponents/sidebar.jsx';
 import ManageProducts from './authComponents/manageProducts.jsx';
+import AddProducts from './authComponents/addProduct.jsx';
+import CategoryManagement from './authComponents/categoryManagement.jsx';
+import AddCategory from './authComponents/addCategory.jsx';
 function App() {
   return (
     <>
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/otp" element={<OtpModal />} />
-      <Route path='/adminProducts' element={<Products />}/>
-      <Route path='/adminCustomers' element= {<Customers />}/>
+      <Route path="/" element={<Home/>} />
+      <Route path="/login" element={<Login/>} />
+      <Route path="/register" element={<Register/>} />
+      <Route path="/otp" element={<OtpModal/>} />
+      <Route path='/adminProducts' element={<Products/>}/>
+      <Route path='/adminCustomers' element= {<Customers/>}/>
       <Route path='/adminSetting' element = {<Setting />}/>
       <Route path='/adminDashboard' element = {<AdminDasboard />}/>
       <Route path='/adminOrder' element = {<Order />}/>
       <Route path='/adminSidebar' element = {<Sidebar/>}/>
       <Route path='/adminManageProducts' element = {<ManageProducts/>}/>
+      <Route path='/addProduct' element = {<AddProducts/>}/>
+      <Route path='/catManagement' element = {<CategoryManagement/>}/>
+      <Route path='/addCategory' element = {<AddCategory/>}/>
+
     </Routes>
     <ToastContainer position="top-right" autoClose={4000} limit={1} />
     </>
