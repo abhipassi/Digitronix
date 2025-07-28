fa978b7efba9575dc0047b50842021d12bd8b0c1
Abhi Passi <abhijit36254@gmail.com>
2025-07-28 14:37:36 +0530

    category management fully working

--- client/src/authComponents/categoryManagement.jsx
+++ client/src/authComponents/categoryManagement.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import Sidebar from './sidebar'
 import axios from 'axios'
+import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 function CategoryManagement() {
   const [category, setCategory] = useState([])
@@ -10,16 +12,35 @@ function CategoryManagement() {
       .then((response) => {
         setCategory(response.data)
       })
-  }, [])
+  }, [category])
 
-    const handleDelete = (e) => {
-      
+    const handleDelete = async(e) => {
+      const categoryName = e.target.value
+      try {
+        // alert(categoryName)
+        const response =  await axios.post("http://localhost:5000/deleteCategory", {categoryName})
+
+        if(response.status == 200){
+          return toast.success("Categroy deleted successfully")
+        }
+      } catch (error) {
+        if(error.response.status === 500){
+          return toast.error("Internal server error")
+        }
+        else{
+          return toast.error("Something went wrong")
+        }
+      }
+
     }
   return (
     <>
       <Sidebar>
         <div className="p-4">
-          <h1 className="text-xl font-bold mb-4">Category Management</h1>
+          <div className='flex justify-between'>
+            <h1 className="text-xl font-bold mb-4">Category Management</h1>
+            <Link to="/addCategory" className='border rounded-3xl mb-1 bg-gray-300 p-2 hover:bg-gray-400'>Add Category</Link>
+          </div>
           <table className="w-full border border-gray-300 text-left">
             <thead className="bg-gray-100">
               <tr>
